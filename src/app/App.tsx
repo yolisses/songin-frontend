336a152a06153fac50f686e5d2ea89517aa5fec1
ulisses <iamurise@gmail.com>
2022-07-27 08:42:22 -0300

    create app domain

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
-function App() {
+export function App() {
   return (
     <div className="App text-4xl">
       hello
     </div>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import './app/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { App } from './app/App';
 import { reportWebVitals } from './config/reportWebVitals';
 
 const root = ReactDOM.createRoot(
