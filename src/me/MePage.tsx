ceab7b2caafde1e5b4db95513c1714872ea8e175
ulisses <iamurise@gmail.com>
2022-07-28 16:56:20 -0300

    add me page carrousel

--- src/me/MePage.tsx
+++ src/me/MePage.tsx
@@ -9,43 +9,49 @@ export function MePage() {
   const iconSize = 22;
   return (
     <>
-      <div className="p-4">
-        <div className="flex flex-row justify-end gap-4">
-          <Link to="/notifications">
-            <FaBell size={iconSize} />
-          </Link>
-          <Link to="/config">
-            <FaCog size={iconSize} />
-          </Link>
-        </div>
-        <div className="flex flex-row items-center gap-2">
-          <img
-            src={user.image}
-            alt={user.name}
-            className="rounded-full bg-gray-200 aspect-square h-20"
-          />
-          <div className="flex flex-col gap-2">
-            <h1 className="text-2xl">
-              {user.name}
-            </h1>
-            <button
-              type="button"
-              className="rounded-full p-1 px-3 pr-2 text-sm bg-black bg-opacity-10"
-            >
-              Perfil
-              <FaChevronRight className="inline pb-[0.1rem]" />
-            </button>
-          </div>
-        </div>
-        <div className="flex flex-row gap-4 mt-6 sticky top-0 bg-white h-12">
-          <button type="button">
-            Favoritas
-          </button>
-          <button type="button">
-            Histórico
+      <div className="flex flex-row justify-end gap-4 p-2">
+        <Link to="/notifications">
+          <FaBell size={iconSize} />
+        </Link>
+        <Link to="/config">
+          <FaCog size={iconSize} />
+        </Link>
+      </div>
+      <div className="flex flex-row items-center gap-2 p-2">
+        <img
+          src={user.image}
+          alt={user.name}
+          className="rounded-full bg-gray-200 aspect-square h-20"
+        />
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl">
+            {user.name}
+          </h1>
+          <button
+            type="button"
+            className="rounded-full p-1 px-3 pr-2 text-sm bg-black bg-opacity-10"
+          >
+            Perfil
+            <FaChevronRight className="inline pb-[0.1rem]" />
           </button>
         </div>
-        <History />
+      </div>
+      <div className="sticky top-0">
+        <div className="absolute flex flex-row gap-4 w-screen top-0 bg-white h-12 items-center px-2">
+          <a href="#favorites">Favoritas</a>
+          <a href="#history">Histórico</a>
+        </div>
+      </div>
+      <div
+        className="flex flex-row overflow-x-auto"
+        style={{ scrollBehavior: 'smooth' }}
+      >
+        <div className="w-screen flex-shrink-0 pt-12 px-2" id="favorites">
+          <History />
+        </div>
+        <div className="w-screen flex-shrink-0 pt-12 px-2" id="history">
+          <History />
+        </div>
       </div>
       <Nav />
     </>
