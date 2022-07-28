99b2c38eccc1979148739730169bfbaecf44a694
ulisses <iamurise@gmail.com>
2022-07-28 08:01:46 -0300

    add redirect by cookie

--- mock/server.mjs
+++ mock/server.mjs
@@ -1,9 +1,11 @@
 import jsonServer from 'json-server';
+import cookieParser from 'cookie-parser';
 
 const server = jsonServer.create();
 
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
+server.use(cookieParser());
 
 server.post('/sign-in', (req, res) => {
   res.cookie('session_id', 'mock_secret_session_id', {
@@ -18,8 +20,30 @@ server.post('/sign-in', (req, res) => {
   });
 });
 
-const router = jsonServer.router('mock/db.json');
-server.use(router);
+server.get('/users/me', (req, res) => {
+  console.log(req.cookies);
+  const sessionId = req.cookies.session_id;
+  if (!sessionId) {
+    res.status(403);
+    res.send('Missing session_id coockie');
+  } else {
+    res.send({
+      name: 'Ulisses',
+      email: 'ulisses@nano.com',
+      image: 'https://picsum.photos/id/338/96/96',
+    });
+  }
+});
+
+server.use(
+  jsonServer.rewriter({
+    '/users/me': '/users/0',
+  }),
+);
+
+server.use(
+  jsonServer.router('mock/db.json'),
+);
 
 const port = 3004;
 server.listen(port, () => {
--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,37 @@
+import axios, { AxiosError } from 'axios';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { api } from './api/api';
 import { HomePage } from './home/HomePage';
 import { SignInPage } from './signIn/SignInPage';
 import { SplashScreen } from './splash/SplashScreen';
 import { useUser } from './user/UserContext';
 
 export function AppRoutes() {
-  const { user } = useUser();
+  const { user, setUser } = useUser();
+
+  const [loading, setLoading] = useState(true);
+
+  async function getMe() {
+    try {
+      const res = await api.get('users/me');
+      setUser(res.data);
+      setLoading(false);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        if (err.response?.status === 403) {
+          setUser(null as any);
+          setLoading(false);
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    getMe();
+  }, []);
+
+  if (loading) return <SplashScreen />;
 
   if (!user) return <SignInPage />;
 
