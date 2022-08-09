91de9f8dbebd33dd0fa54af2217aef9f404b0296
ulisses <iamurise@gmail.com>
2022-08-09 11:21:35 -0300

    styling

--- src/me/MePage.tsx
+++ src/me/MePage.tsx
@@ -9,31 +9,33 @@ export function MePage() {
   const iconSize = 22;
   return (
     <div className="">
-      <div className="flex flex-row justify-end gap-4 p-2">
-        <Link to="/notifications">
-          <FaBell size={iconSize} />
-        </Link>
-        <Link to="/config">
-          <FaCog size={iconSize} />
-        </Link>
-      </div>
       <div className="flex flex-row items-center gap-2 p-2">
         <img
           src={user.image}
           alt={user.name}
           className="rounded-full bg-gray-200 aspect-square h-20"
         />
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 flex-1">
           <h1 className="text-2xl">
             {user.name}
           </h1>
-          <button
-            type="button"
-            className="rounded-full p-1 px-3 pr-2 text-sm bg-black bg-opacity-10"
-          >
-            Perfil
-            <FaChevronRight className="inline pb-[0.1rem]" />
-          </button>
+          <div>
+            <button
+              type="button"
+              className="rounded-full inline-block p-1 px-3 pr-2 text-sm bg-black bg-opacity-10"
+            >
+              Perfil
+              <FaChevronRight className="inline pb-[0.1rem]" />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-row self-start gap-4 p-2">
+          <Link to="/notifications">
+            <FaBell size={iconSize} />
+          </Link>
+          <Link to="/config">
+            <FaCog size={iconSize} />
+          </Link>
         </div>
       </div>
       <div className="sticky top-0 flex flex-row gap-4 bg-white h-12 items-center">
