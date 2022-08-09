c7ccd30b4c2873b46566affb17d0c3dbac72c938
ulisses <iamurise@gmail.com>
2022-08-09 09:35:28 -0300

    add nav to player page

--- src/player/PlayerPage.tsx
+++ src/player/PlayerPage.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import { FaComment, FaShare } from 'react-icons/fa';
 import { api } from '../api/api';
-import { useColors } from '../dev/utils/useColors';
 import { LikeButton } from '../like/LikeButton';
 import { Music } from '../music/Music';
+import { Nav } from '../nav/Nav';
 import { GradientBackground } from './GradientBackground';
 import { PlayerRange } from './PlayerRange';
 
@@ -25,33 +25,41 @@ export function PlayerPage() {
       <GradientBackground />
       <div className="flex flex-col h-screen overflow-y-scroll no-scrollbar snap-y snap-mandatory text-white">
         {musics && musics.map((music) => (
+
           <div
             key={music.id}
-            className="h-screen snap-start flex-shrink-0"
+            className="h-screen snap-start flex-shrink-0 relative"
           >
-            <div className="px-4 flex flex-col gap-6">
-              <div className="flex flex-col gap-2">
-                <div className="text-xl">
-                  {music.name}
+            <div
+              className="bottom-0 absolute w-screen"
+            >
+              <div className="px-4 flex flex-col gap-6">
+                <div className="flex flex-col gap-2">
+                  <div className="text-xl">
+                    {music.name}
+                  </div>
+                  <div className="">
+                    {music.artist?.name}
+                  </div>
                 </div>
-                <div className="">
-                  {music.artist?.name}
+                <div className="flex flex-row gap-6">
+                  <LikeButton />
+                  <button type="button">
+                    <FaComment size={iconsSize} />
+                  </button>
+                  <button type="button">
+                    <FaShare size={iconsSize} />
+                  </button>
                 </div>
+                <PlayerRange />
               </div>
-              <div className="flex flex-row gap-6">
-                <LikeButton />
-                <button type="button">
-                  <FaComment size={iconsSize} />
-                </button>
-                <button type="button">
-                  <FaShare size={iconsSize} />
-                </button>
-              </div>
-              <PlayerRange />
+              <div className="h-16" />
             </div>
-            <div className="h-16" />
           </div>
         ))}
+      </div>
+      <div className="text-white absolute bottom-0">
+        <Nav white={false} />
       </div>
     </>
   );
