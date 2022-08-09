84191fb453ae24eee9c7d1202ee7656956ab5db6
ulisses <iamurise@gmail.com>
2022-08-09 09:29:19 -0300

    add music info in player page

--- src/player/PlayerPage.tsx
+++ src/player/PlayerPage.tsx
@@ -1,22 +1,55 @@
+import { useEffect, useState } from 'react';
+import { FaComment, FaShare } from 'react-icons/fa';
+import { api } from '../api/api';
 import { useColors } from '../dev/utils/useColors';
+import { LikeButton } from '../like/LikeButton';
+import { Music } from '../music/Music';
 import { GradientBackground } from './GradientBackground';
 import { PlayerRange } from './PlayerRange';
 
 export function PlayerPage() {
-  const colors = useColors(10);
+  const [musics, setMusics] = useState<Music[]>();
+  const iconsSize = 24;
+
+  async function getMusics() {
+    const res = await api.get('/musics');
+    setMusics(res.data);
+  }
+
+  useEffect(() => {
+    getMusics();
+  }, []);
 
   return (
     <>
       <GradientBackground />
-      <div className="flex flex-col h-screen overflow-y-scroll no-scrollbar snap-y snap-mandatory">
-        {colors.map((color) => (
+      <div className="flex flex-col h-screen overflow-y-scroll no-scrollbar snap-y snap-mandatory text-white">
+        {musics && musics.map((music) => (
           <div
-            key={color}
-            style={{ backgroundColor: color }}
+            key={music.id}
             className="h-screen snap-start flex-shrink-0"
           >
-            {color}
-            <PlayerRange />
+            <div className="px-4 flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <div className="text-xl">
+                  {music.name}
+                </div>
+                <div className="">
+                  {music.artist?.name}
+                </div>
+              </div>
+              <div className="flex flex-row gap-6">
+                <LikeButton />
+                <button type="button">
+                  <FaComment size={iconsSize} />
+                </button>
+                <button type="button">
+                  <FaShare size={iconsSize} />
+                </button>
+              </div>
+              <PlayerRange />
+            </div>
+            <div className="h-16" />
           </div>
         ))}
       </div>
