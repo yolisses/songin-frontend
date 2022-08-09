6dc4e24344fa8888c7df01e31445d34b9328a943
ulisses <iamurise@gmail.com>
2022-08-09 17:09:17 -0300

    separate player card

---
+++ src/player/PlayerCard.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import { FaComment, FaShare } from 'react-icons/fa';
+import { Ballon } from './Ballon';
+import { Music } from '../music/Music';
+import { PlayerRange } from './PlayerRange';
+import { LikeButton } from '../like/LikeButton';
+import { FloatingCounter } from './FloatingCounter';
+import { ShareButtons } from '../share/ShareButtons';
+
+interface PlayerCardProps{
+    music:Music
+}
+
+export function PlayerCard({ music }:PlayerCardProps) {
+  const [share, setShare] = useState(false);
+  const iconsSize = 24;
+
+  function handleShareClick() {
+    console.log(share);
+    setShare((value) => !value);
+  }
+
+  return (
+    <div
+      key={music.id}
+      className="h-screen w-screen overflow-hidden snap-start flex-shrink-0 relative"
+    >
+      <div
+        className="absolute -z-40 w-screen h-screen bg-cover bg-center bg-no-repeat blur-lg scale-110"
+        style={{ backgroundImage: `url("${music.image}")` }}
+      />
+      <img
+        src={music.image}
+        alt={music.name}
+        className="mask absolute -z-30 w-screen aspect-square max-w-[100vh]
+-translate-y-1/2 top-1/2 translate-x-1/2 right-1/2 "
+      />
+      <div
+        className="bottom-0 absolute w-screen"
+      >
+        <div className="px-4 flex flex-col gap-4">
+          <div className="flex flex-col pb-2">
+            <div className="text-xl">
+              {music.name}
+            </div>
+            <div className="">
+              {music.artist?.name}
+            </div>
+          </div>
+          <div className="overflow-hidden">
+            {share && (
+            <Ballon>
+              <ShareButtons
+                size={40}
+                url={`https://musiks.com/music/${music.id}`}
+              />
+            </Ballon>
+            )}
+          </div>
+          <div className="flex flex-row gap-8">
+            <LikeButton music={music} />
+            <FloatingCounter count={130}>
+              <button type="button">
+                <FaComment size={iconsSize} />
+              </button>
+            </FloatingCounter>
+            <FloatingCounter count={130}>
+              <button
+                type="button"
+                onClick={handleShareClick}
+                className="active:scale-50 transition"
+              >
+                <FaShare size={iconsSize} />
+              </button>
+            </FloatingCounter>
+          </div>
+          <PlayerRange />
+        </div>
+        <div className="h-16" />
+      </div>
+    </div>
+  );
+}
--- src/player/PlayerPage.tsx
+++ src/player/PlayerPage.tsx
@@ -1,19 +1,12 @@
 import { useEffect, useState } from 'react';
-import { FaComment, FaShare } from 'react-icons/fa';
-import { api } from '../api/api';
-import { LikeButton } from '../like/LikeButton';
-import { Music } from '../music/Music';
 import { Nav } from '../nav/Nav';
-import { ShareButtons } from '../share/ShareButtons';
-import { Ballon } from './Ballon';
-import { FloatingCounter } from './FloatingCounter';
+import { api } from '../api/api';
+import { Music } from '../music/Music';
+import { PlayerCard } from './PlayerCard';
 import { GradientBackground } from './GradientBackground';
-import { PlayerRange } from './PlayerRange';
 
 export function PlayerPage() {
   const [musics, setMusics] = useState<Music[]>();
-  const [share, setShare] = useState(false);
-  const iconsSize = 24;
 
   async function getMusics() {
     const res = await api.get('/musics');
@@ -29,60 +22,7 @@ export function PlayerPage() {
       <GradientBackground />
       <div className="flex flex-col h-screen py-20 overflow-y-scroll no-scrollbar snap-y snap-mandatory text-white">
         {musics && musics.map((music) => (
-          <div
-            key={music.id}
-            className="h-screen w-screen overflow-hidden snap-start flex-shrink-0 relative"
-          >
-            <div
-              className="absolute -z-40 w-screen h-screen bg-cover bg-center bg-no-repeat blur-lg scale-110"
-              style={{ backgroundImage: `url("${music.image}")` }}
-            />
-            <img
-              src={music.image}
-              alt={music.name}
-              className="mask absolute -z-30 w-screen aspect-square max-w-[100vh]
-        -translate-y-1/2 top-1/2 translate-x-1/2 right-1/2 "
-            />
-            <div
-              className="bottom-0 absolute w-screen"
-            >
-              <div className="px-4 flex flex-col gap-4">
-                <div className="flex flex-col pb-2">
-                  <div className="text-xl">
-                    {music.name}
-                  </div>
-                  <div className="">
-                    {music.artist?.name}
-                  </div>
-                </div>
-                <div className="overflow-hidden">
-                  {share && (
-                    <Ballon>
-                      <ShareButtons
-                        size={40}
-                        url={`https://musiks.com/music/${music.id}`}
-                      />
-                    </Ballon>
-                  )}
-                </div>
-                <div className="flex flex-row gap-8">
-                  <LikeButton music={music} />
-                  <FloatingCounter count={130}>
-                    <button type="button">
-                      <FaComment size={iconsSize} />
-                    </button>
-                  </FloatingCounter>
-                  <FloatingCounter count={130}>
-                    <button type="button" onClick={() => setShare((value) => !value)}>
-                      <FaShare size={iconsSize} />
-                    </button>
-                  </FloatingCounter>
-                </div>
-                <PlayerRange />
-              </div>
-              <div className="h-16" />
-            </div>
-          </div>
+          <PlayerCard music={music} />
         ))}
       </div>
       <div className="absolute bottom-0 -z-10 w-screen h-60 bg-gradient-to-t from-[#000d] via-[#000a] to-transparent" />
