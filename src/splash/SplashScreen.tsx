a42efb46e77d7d79c060d2db03c3a5d3c74b873d
Ulisses <iamurise@gmail.com>
2022-09-06 09:42:12 -0300

    change logo

--- src/splash/SplashScreen.tsx
+++ src/splash/SplashScreen.tsx
@@ -2,7 +2,7 @@ export function SplashScreen() {
   return (
     <div className="width-screen h-screen flex justify-center items-center flex-col">
       <div className="my-auto flex flex-col items-center gap-4">
-        <img src="logo/gradient.svg" alt="logo" width={100} />
+        <img src="logo/gradient.svg" alt="logo" width={86} />
         <h1 className="text-3xl font-bold bg-gradient-to-tr from-sky-500 to-purple-500 bg-clip-text text-transparent">Sonhin</h1>
       </div>
     </div>
