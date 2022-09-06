13073f3a4a95ee5fe939d8118dbbc80d0cb38b61
Ulisses <iamurise@gmail.com>
2022-09-06 09:45:37 -0300

    style floating title

--- src/common/FloatingTitle.tsx
+++ src/common/FloatingTitle.tsx
@@ -3,9 +3,9 @@ import { ChildrenProps } from './ChildrenProps';
 export function FloatingTitle({ children }:ChildrenProps) {
   return (
     <div className="h-20 w-full">
-      <div className="h-20 top-0 font-light text-4xl flex flex-row items-center w-full bg-white z-10">
+      <h2 className="h-16 top-0 font-light text-4xl flex flex-row items-center w-full bg-white z-10">
         {children}
-      </div>
+      </h2>
     </div>
 
   );
