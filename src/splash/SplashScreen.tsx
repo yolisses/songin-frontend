ebddfb8238b061f67dd7242745bfe66406bce0a9
Ulisses <iamurise@gmail.com>
2022-09-06 10:43:13 -0300

    fix splash image background

--- src/splash/SplashScreen.tsx
+++ src/splash/SplashScreen.tsx
@@ -2,7 +2,12 @@ export function SplashScreen() {
   return (
     <div className="width-screen h-screen flex justify-center items-center flex-col">
       <div className="my-auto flex flex-col items-center gap-4">
-        <img src="logo/gradient.svg" alt="logo" width={86} />
+        <img
+          alt="logo"
+          width={86}
+          src="logo/gradient.svg"
+          className="bg-transparent"
+        />
         <h1 className="text-3xl font-bold bg-gradient-to-tr from-sky-500 to-purple-500 bg-clip-text text-transparent">Sonhin</h1>
       </div>
     </div>
