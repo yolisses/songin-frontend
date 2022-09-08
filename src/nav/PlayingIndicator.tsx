b8aadf1db41969d32cf6bc0485f6a27defbdb89f
Ulisses <iamurise@gmail.com>
2022-09-08 10:49:45 -0300

    make playing indicator rounded

--- src/nav/PlayingIndicator.tsx
+++ src/nav/PlayingIndicator.tsx
@@ -2,34 +2,32 @@ interface PlayingIndicatorProps{
     playing:boolean
 }
 
-export function PlayingIndicator({ playing }:PlayingIndicatorProps) {
+interface IndicatorBarProps{
+  delay:number
+  duration:number
+  playing:boolean
+}
+
+function IndicatorBar({ delay, duration, playing }:IndicatorBarProps) {
   const playState = playing ? 'running' : 'paused';
   return (
+    <div
+      className="w-1 h-5 bg-white animate-spring rounded-full"
+      style={{
+        animationDelay: `${delay}s`,
+        animationPlayState: playState,
+        animationDuration: `${duration}s`,
+      }}
+    />
+  );
+}
+
+export function PlayingIndicator({ playing }:PlayingIndicatorProps) {
+  return (
     <div className="flex flex-row items-center h-5 gap-[0.15rem]">
-      <div
-        className="w-1 h-5 bg-black animate-spring"
-        style={{
-          animationDelay: '0s',
-          animationDuration: '0.8s',
-          animationPlayState: playState,
-        }}
-      />
-      <div
-        className="w-1 h-5 bg-black animate-spring"
-        style={{
-          animationDelay: '0.4s',
-          animationDuration: '0.9s',
-          animationPlayState: playState,
-        }}
-      />
-      <div
-        className="w-1 h-5 bg-black animate-spring"
-        style={{
-          animationDelay: '0s',
-          animationDuration: '0.7s',
-          animationPlayState: playState,
-        }}
-      />
+      <IndicatorBar delay={0} duration={0.8} playing={playing} />
+      <IndicatorBar delay={0.4} duration={0.9} playing={playing} />
+      <IndicatorBar delay={0} duration={0.7} playing={playing} />
     </div>
   );
 }
