4227548eed6ab78b110bad2ca74c4376e461a898
Ulisses <iamurise@gmail.com>
2022-09-16 09:01:56 -0300

    separate music table item

--- src/common/MusicTable.tsx
+++ src/common/MusicTable.tsx
@@ -1,83 +1,21 @@
-import { Link } from 'react-router-dom';
-import { Music } from '../music/Music';
-import { useMusics } from '../music/MusicsContext';
-import { formatMusicTime } from './formatMusicTime';
 import { repeat } from './repeat';
+import { Music } from '../music/Music';
+import { MusicTableItem } from './MusicTableItem';
 
 interface MusicTableProps{
   musics?:Music[]
-}
-
-interface MusicTableLineProps{
-  music?:Music
-}
-
-function MusicTableLine({ music }:MusicTableLineProps) {
-  const { setMusic } = useMusics();
-  const loading = music === undefined;
-
-  function handleClick() {
-    if (music) {
-      setMusic(music);
-    }
-  }
-
-  return (
-    <tr className="active-opacity rounded-lg">
-      <td className="mr-2 w-12">
-        {loading ? (
-          <div
-            className="gradient-loading aspect-square rounded w-12"
-          />
-        )
-          : (
-            <img
-              alt={music.name}
-              src={music.image}
-              className="aspect-square rounded w-12"
-            />
-          )}
-      </td>
-      <td>
-        {loading ? <div className="gradient-loading w-32">&nbsp;</div>
-          : (
-            <button
-              onClick={handleClick}
-              className="text-left hover:underline py-2"
-            >
-              {music.name}
-            </button>
-          )}
-      </td>
-      <td>
-        {loading ? <div className="gradient-loading w-32">&nbsp;</div> : (
-          <Link
-            to={`/@${music.artist?.name}`}
-            className="opacity-60 hover:underline"
-          >
-            {music.artist?.name || '&nbsp;'}
-          </Link>
-        )}
-      </td>
-      <td className="opacity-60 pr-2">
-        {loading
-          ? <div className="gradient-loading w-10">&nbsp;</div>
-          : formatMusicTime(music.duration)}
-      </td>
-    </tr>
-  );
 }
 
 export function MusicTable({ musics }:MusicTableProps) {
   return (
     <table className="w-full table-auto border-collapse">
       {musics ? musics.map((music) => (
-        <MusicTableLine
+        <MusicTableItem
           music={music}
           key={music.id}
         />
       ))
-        : repeat(<MusicTableLine />, 10)}
+        : repeat(<MusicTableItem />, 10)}
     </table>
   );
 }
---
+++ src/common/MusicTableItem.tsx
@@ -0,0 +1,64 @@
+import { Link } from 'react-router-dom';
+import { Music } from '../music/Music';
+import { useMusics } from '../music/MusicsContext';
+import { formatMusicTime } from './formatMusicTime';
+
+interface MusicTableItemProps{
+    music?:Music
+  }
+
+export function MusicTableItem({ music }:MusicTableItemProps) {
+  const { setMusic } = useMusics();
+  const loading = music === undefined;
+
+  function handleClick() {
+    if (music) {
+      setMusic(music);
+    }
+  }
+
+  return (
+    <tr className="active-opacity rounded-lg">
+      <td className="mr-2 w-12">
+        {loading ? (
+          <div
+            className="gradient-loading aspect-square rounded w-12"
+          />
+        )
+          : (
+            <img
+              alt={music.name}
+              src={music.image}
+              className="aspect-square rounded w-12"
+            />
+          )}
+      </td>
+      <td>
+        {loading ? <div className="gradient-loading w-32">&nbsp;</div>
+          : (
+            <button
+              onClick={handleClick}
+              className="text-left hover:underline py-2"
+            >
+              {music.name}
+            </button>
+          )}
+      </td>
+      <td>
+        {loading ? <div className="gradient-loading w-32">&nbsp;</div> : (
+          <Link
+            to={`/@${music.artist?.name}`}
+            className="opacity-60 hover:underline"
+          >
+            {music.artist?.name || '&nbsp;'}
+          </Link>
+        )}
+      </td>
+      <td className="opacity-60 pr-2">
+        {loading
+          ? <div className="gradient-loading w-10">&nbsp;</div>
+          : formatMusicTime(music.duration)}
+      </td>
+    </tr>
+  );
+}
