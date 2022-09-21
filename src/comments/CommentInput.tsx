1795a93f51c01daed5702c3015446a99236f3862
Ulisses <iamurise@gmail.com>
2022-09-21 16:49:26 -0300

    add form tag to comment input

--- src/comments/CommentInput.tsx
+++ src/comments/CommentInput.tsx
@@ -1,16 +1,34 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { FaPaperPlane } from 'react-icons/fa';
 
 export function CommentInput() {
+  const [text, setText] = useState('');
+
+  function handleSubmit(e:FormEvent) {
+    e.preventDefault();
+    alert(text);
+  }
+
+  function handleChange(e:ChangeEvent<HTMLInputElement>) {
+    const text = e.target.value;
+    setText(text);
+  }
+
   return (
-    <div className="w-full flex flex-row gap-2 p-2 items-center">
+    <form
+      onSubmit={handleSubmit}
+      className="w-full flex flex-row gap-2 p-2 items-center"
+    >
       <input
         type="text"
+        value={text}
+        onChange={handleChange}
         placeholder="So, what do think?"
         className="p-2 bg-transparent flex-1 rounded-full border-2 border-opacity-50"
       />
       <button className="p-2 text-lg group">
         <FaPaperPlane className="group-active:scale-50 transition-transform" />
       </button>
-    </div>
+    </form>
   );
 }
