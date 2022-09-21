3ba38c8f6fd070c058abee2738cca44beeaabf03
Ulisses <iamurise@gmail.com>
2022-09-21 16:41:09 -0300

    remove DEV title for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { Helmet } from 'react-helmet';
 import { Routes } from './Routes';
-import { isDev } from './common/isDev';
 import { UserContextProvider } from './user/UserContext';
 import { HomeContextProvider } from './home/HomeContext';
 import { ModalContextProvider } from './modal/ModalContext';
@@ -9,23 +7,16 @@ import { PlayerContextProvider } from './player/PlayerContext';
 
 export function App() {
   return (
-    <>
-      {isDev && (
-      <Helmet>
-        <title>DEV Sonhin</title>
-      </Helmet>
-      )}
-      <UserContextProvider>
-        <MusicsContextProvider>
-          <PlayerContextProvider>
-            <ModalContextProvider>
-              <HomeContextProvider>
-                <Routes />
-              </HomeContextProvider>
-            </ModalContextProvider>
-          </PlayerContextProvider>
-        </MusicsContextProvider>
-      </UserContextProvider>
-    </>
+    <UserContextProvider>
+      <MusicsContextProvider>
+        <PlayerContextProvider>
+          <ModalContextProvider>
+            <HomeContextProvider>
+              <Routes />
+            </HomeContextProvider>
+          </ModalContextProvider>
+        </PlayerContextProvider>
+      </MusicsContextProvider>
+    </UserContextProvider>
   );
 }
