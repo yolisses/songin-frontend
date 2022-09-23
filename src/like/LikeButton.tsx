0a6a9d4531cb323668dcebaa5ad1153c78ee6d49
Ulisses <iamurise@gmail.com>
2022-09-23 15:25:09 -0300

    refactor like button

--- src/like/LikeButton.tsx
+++ src/like/LikeButton.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { FaHeart } from 'react-icons/fa';
 import { Music } from '../music/Music';
+import { LikeButtonContent } from './LikeButtonContent';
 import { FloatingCounter } from '../player/FloatingCounter';
 
 interface LikeButtonProps{
@@ -11,14 +11,6 @@ interface LikeButtonProps{
 export function LikeButton({ music, alreadyLiked }:LikeButtonProps) {
   const [animate, setAnimate] = useState(false);
   const [active, setActive] = useState(alreadyLiked);
-
-  let add = 0;
-  if (alreadyLiked && !active) {
-    add = -1;
-  }
-  if (!alreadyLiked && active) {
-    add = 1;
-  }
 
   function handleClick() {
     setActive((old) => {
@@ -31,26 +23,17 @@ export function LikeButton({ music, alreadyLiked }:LikeButtonProps) {
     setActive(alreadyLiked);
   }, [alreadyLiked]);
 
-  const iconsSize = 24;
   return (
-    <FloatingCounter count={music && (music.likesCount + add)}>
+    <FloatingCounter
+      count={music?.likesCount}
+    >
       <button
         onClick={handleClick}
         className="group relative p-2"
       >
-        <FaHeart
-          size={iconsSize}
-          className="group-active:scale-50 transition-transform absolute focus:outline-none animate-explode select-none pointer-events-none"
-          style={{
-            color: 'red',
-            opacity: animate ? '100%' : '0',
-            animationName: animate ? undefined : 'animate-explode',
-          }}
-        />
-        <FaHeart
-          size={iconsSize}
-          className="group-active:scale-50 transition-transform"
-          style={{ color: active ? 'red' : undefined }}
+        <LikeButtonContent
+          active={active}
+          animate={animate}
         />
       </button>
     </FloatingCounter>
---
+++ src/like/LikeButtonContent.tsx
@@ -0,0 +1,28 @@
+import { FaHeart } from 'react-icons/fa';
+
+interface Props{
+    active:boolean
+    animate:boolean
+}
+
+export function LikeButtonContent({ active, animate }:Props) {
+  const iconsSize = 24;
+  return (
+    <div>
+      <FaHeart
+        size={iconsSize}
+        className="group-active:scale-50 transition-transform absolute focus:outline-none animate-explode select-none pointer-events-none"
+        style={{
+          color: 'red',
+          opacity: animate ? '100%' : '0',
+          animationName: animate ? undefined : 'animate-explode',
+        }}
+      />
+      <FaHeart
+        size={iconsSize}
+        className="group-active:scale-50 transition-transform"
+        style={{ color: active ? 'red' : undefined }}
+      />
+    </div>
+  );
+}
